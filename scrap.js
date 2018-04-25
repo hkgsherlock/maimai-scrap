f7f3937d60552855c92c6b23e4e82db6fbc6f478
hkgsherlock <hkgsherlock@gmail.com>
2018-04-25 23:41:44 +0800

    debug

--- scrap.js
+++ scrap.js
@@ -8,30 +8,39 @@ const {
 } = require('selenium-webdriver');
 const ChromeDriver = require('selenium-webdriver/chrome');
 
+const scrapProfile = require('./scrapProfile.js');
+
 const options = new ChromeDriver.Options();
 options.addArguments(
-    'headless',
+    // 'headless',
 	"proxy-server='direct://'",
 	"proxy-bypass-list=*",
     'disable-infobars',
     'disable-gpu',
-    "window-size=1200x600",
+    "window-size=360x640",
     `user-data-dir=${path.resolve(__dirname, "chrome_userdata")}`,
 );
 
 (async function example() {
-    console.log("browser building...");
     let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
     try {
-        console.log("browser built, re-directing");
-        // await driver.get('https://maimai-net.com/maimai-mobile/home/');
         await driver.get('https://maimai-net.com/maimai-mobile/home/');
-        console.log('yay');
 
-        const body = await driver.findElement(By.css('body'));
-        console.log(await body.getText());
+        await driver.executeScript([
+    		`document.querySelector('form > input[name="segaId"]').value = "soumasandesu";`,
+    		`document.querySelector('form > input[name="passWd"]').value = "958746aA";`,
+    	].join(''));
+
+        const form = await driver.findElement(By.css('form'));
+        await form.submit();
+
+        // const body = await driver.findElement(By.css('body'));
+        // console.log(await body.getText());
+        console.log(await scrapProfile(driver));
+    } catch(e) {
+        console.error("eew, ", e);
     } finally {
-        console.log("quit");
+        console.warn("quit");
         await driver.quit();
     }
 })();
